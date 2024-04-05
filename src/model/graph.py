6903108d81aac8b80871de10c900c23810abb176
Randy <randomzakya@gmail.com>
2024-04-05 15:14:50 +0700

    add refer for mistral repo

--- src/model/graph.py
+++ src/model/graph.py
@@ -1,13 +1,21 @@
 import torch
 from llama import Transformer, Tokenizer
 
-class SubgraphGenerator(torch.nn.Module):
+class SubgraphPooler(torch.nn.Module):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         self.encoder = None
         self.lm = None
         self.subgraph_pooler = None
-        self.graph_decoder = None
+    
+    def forward(self, text_input_ids, entities_input_ids, relations_input_ids, x_coo, coo_mask=None):
+        pass
+
+class SubgraphGenerator(torch.nn.Module):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.subgraph_pooler = SubgraphPooler()
+        self.decoder = None
     
     def forward(self, text_input_ids, entities_input_ids, relations_input_ids, x_coo, coo_mask=None):
         pass
---
+++ src/model/lm.py
@@ -0,0 +1,3 @@
+from transformers import MistralModel, MistralForCausalLM
+
+# https://github.com/huggingface/transformers/blob/main/src/transformers/models/mistral/modeling_mistral.py
