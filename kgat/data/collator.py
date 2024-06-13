8d5dca3de001bf3d2db3e8d5ee9314bb04740ae4
Randy <randomzakya@gmail.com>
2024-06-13 13:49:16 +0700

    refactor data collator for sg

--- kgat/data/collator.py
+++ kgat/data/collator.py
@@ -1,14 +1,18 @@
 import torch
+from torch.nn.utils.rnn import pad_sequence
+import numpy as np
 
 def flatten(list_of_lists):
     return [item for sublist in list_of_lists for item in sublist]
 
 
 class SubgraphGenerationCollator:
-    def __init__(self, tokenizer):
+    def __init__(self, tokenizer, n_process, left=True):
         self.tokenizer = tokenizer
+        self.n_process = n_process
+        self.left = left
 
-    def __call__(self, batch):
+    def call_per_process(self, batch):
         text, entities, relations, x_coo, y_coo_cls = zip(*batch)
 
         text = list(text)
@@ -54,17 +58,73 @@ class SubgraphGenerationCollator:
         x_coo = x_coo.transpose(0,1)
 
         return {
-            "graph_query_input_ids" : graph_query["input_ids"],
-            "graph_query_attention_mask" : graph_query["attention_mask"],
-            "entities_input_ids" : entities["input_ids"],
-            "entities_attention_mask" : entities["attention_mask"],
-            "relations_input_ids" : relations["input_ids"],
-            "relations_attention_mask" : relations["attention_mask"],
-            "x_coo" : x_coo,
-            "batch" : batch,
-            "y_coo_cls" : y_coo_cls
+            "graph_query_input_ids" : graph_query["input_ids"], # N_query, length
+            "graph_query_attention_mask" : graph_query["attention_mask"], # N_query, length
+            "entities_input_ids" : entities["input_ids"], # N_entities, length
+            "entities_attention_mask" : entities["attention_mask"], # N_entities, length
+            "relations_input_ids" : relations["input_ids"], # N_relations, length
+            "relations_attention_mask" : relations["attention_mask"], # N_relations, length
+            "x_coo" : x_coo, # N_triplets, 3
+            "batch" : batch, # N_entities
+            "y_coo_cls" : y_coo_cls # N_triplets
         }
     
+    def pad_batch_input_ids(self, batch_input_ids):
+        res = []
+        lengths = [0]
+        for input_ids in batch_input_ids:
+            res.extend(input_ids.flip(-1) if self.left else input_ids)
+            lengths.append(input_ids.shape[0])
+        res = pad_sequence(res, batch_first=True, padding_value=self.tokenizer.pad_token_id)
+        res = res.flip(-1) if self.left else res
+        res = [res[lengths[i]:lengths[i+1]] for i in range(len(batch_input_ids))]
+        return res
+
+    def pad_batch_attention_mask(self, batch_attention_masks):
+        res = []
+        lengths = [0]
+        for attention_masks in batch_attention_masks:
+            res.extend(attention_masks.flip(-1) if self.left else attention_masks)
+            lengths.append(attention_masks.shape[0])
+        res = pad_sequence(res, batch_first=True, padding_value=0)
+        res = res.flip(-1) if self.left else res
+        res = [res[lengths[i]:lengths[i+1]] for i in range(len(batch_attention_masks))]
+        return res
+    
+    def __call__(self, batch):
+        pad_tokens = {
+            "graph_query_input_ids" : self.tokenizer.pad_token_id, # SUCCESS
+            "graph_query_attention_mask" : 0, # SUCCESS
+            "entities_input_ids" : self.tokenizer.pad_token_id, # SUCCESS
+            "entities_attention_mask" : 0, # SUCCESS
+            "relations_input_ids" : self.tokenizer.pad_token_id, # SUCCESS
+            "relations_attention_mask" : 0, # SUCCESS
+            "x_coo" : -1, # SUCCESS
+            "batch" : -1, # SUCCESS
+            "y_coo_cls" : -1, # SUCCESS
+        }
+        n = max(1, int(np.ceil(len(batch) / self.n_process)))
+        
+        batch = [batch[i:i+n] for i in range(0,len(batch),n)]
+
+        batch = [self.call_per_process(el) for el in batch]
+
+        keys = pad_tokens.keys()
+        
+        res = {}
+        
+        for k in keys:
+            batch_values = [el[k] for el in batch]
+            if "input_ids" in k:
+                batch_values = self.pad_batch_input_ids(batch_values)
+            elif "attention_mask" in k:
+                batch_values = self.pad_batch_attention_mask(batch_values)
+            batch_values = pad_sequence(batch_values, batch_first=True, padding_value=pad_tokens[k])
+            batch_values = batch_values.view(-1) if k == "batch" or k == "y_coo_cls" else batch_values.view(-1, batch_values.shape[-1])
+            res[k] = batch_values
+        return res
+
+
 class LMKBCCollator:
     def __init__(self, tokenizer):
         self.tokenizer = tokenizer
