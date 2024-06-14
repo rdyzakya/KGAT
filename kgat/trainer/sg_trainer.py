f46bb7d74dc1dba6610f6c101d04d8790f44fab8
Randy <randomzakya@gmail.com>
2024-06-14 23:16:40 +0700

    add pred step

--- kgat/trainer/sg_trainer.py
+++ kgat/trainer/sg_trainer.py
@@ -28,23 +28,115 @@ class SGTrainer(Trainer):
         # y_coo = batch[5]
 
         # text, entities, relations, x_coo, y_coo_cls
-        label = inputs.pop("y_coo_cls")
+        labels = inputs.pop("y_coo_cls")
         mean_fused_score, _, _ = model(**inputs)
 
-        assert len(mean_fused_score) == len(label)
+        assert len(mean_fused_score) == len(labels)
 
-        mask = label != -1
+        mask = labels != -1
 
         mean_fused_score = mean_fused_score[mask]
-        label = label[mask]
+        labels = labels[mask]
 
-        label = label.float()
+        labels = labels.float()
 
-        loss = self.criterion(mean_fused_score, label)
+        loss = self.criterion(mean_fused_score, labels)
 
         if return_outputs:
             return loss, mean_fused_score
         return loss
+    
+    def prediction_step(
+        self,
+        model,
+        inputs,
+        prediction_loss_only,
+        ignore_keys = None,
+    ):
+        # has_labels = False if len(self.label_names) == 0 else all(inputs.get(k) is not None for k in self.label_names)
+        # # For CLIP-like models capable of returning loss values.
+        # # If `return_loss` is not specified or being `None` in `inputs`, we check if the default value of `return_loss`
+        # # is `True` in `model.forward`.
+        # return_loss = inputs.get("return_loss", None)
+        # if return_loss is None:
+        #     return_loss = self.can_return_loss
+        # loss_without_labels = True if len(self.label_names) == 0 and return_loss else False
+
+        # inputs = self._prepare_inputs(inputs)
+        # if ignore_keys is None:
+        #     if hasattr(self.model, "config"):
+        #         ignore_keys = getattr(self.model.config, "keys_to_ignore_at_inference", [])
+        #     else:
+        #         ignore_keys = []
+
+        # labels may be popped when computing the loss (label smoothing for instance) so we grab them first.
+        # if has_labels or loss_without_labels:
+        #     labels = nested_detach(tuple(inputs.get(name) for name in self.label_names))
+        #     if len(labels) == 1:
+        #         labels = labels[0]
+        # else:
+        #     labels = None
+        labels = inputs.pop("y_coo_cls")
+
+        mask = labels != -1
+        labels = labels[mask]
+
+        labels = labels.float()
+
+
+        with torch.no_grad():
+            mean_fused_score, _, _ = model(**inputs)
+            # if is_sagemaker_mp_enabled():
+            #     raw_outputs = smp_forward_only(model, inputs)
+            #     if has_labels or loss_without_labels:
+            #         if isinstance(raw_outputs, dict):
+            #             loss_mb = raw_outputs["loss"]
+            #             logits_mb = tuple(v for k, v in raw_outputs.items() if k not in ignore_keys + ["loss"])
+            #         else:
+            #             loss_mb = raw_outputs[0]
+            #             logits_mb = raw_outputs[1:]
+
+            #         loss = loss_mb.reduce_mean().detach().cpu()
+            #         logits = smp_nested_concat(logits_mb)
+            #     else:
+            #         loss = None
+            #         if isinstance(raw_outputs, dict):
+            #             logits_mb = tuple(v for k, v in raw_outputs.items() if k not in ignore_keys)
+            #         else:
+            #             logits_mb = raw_outputs
+            #         logits = smp_nested_concat(logits_mb)
+            # else:
+            #     if has_labels or loss_without_labels:
+            #         with self.compute_loss_context_manager():
+            #             loss, outputs = self.compute_loss(model, inputs, return_outputs=True)
+            #         loss = loss.mean().detach()
+
+            #         if isinstance(outputs, dict):
+            #             logits = tuple(v for k, v in outputs.items() if k not in ignore_keys + ["loss"])
+            #         else:
+            #             logits = outputs[1:]
+            #     else:
+            #         loss = None
+            #         with self.compute_loss_context_manager():
+            #             outputs = model(**inputs)
+            #         if isinstance(outputs, dict):
+            #             logits = tuple(v for k, v in outputs.items() if k not in ignore_keys)
+            #         else:
+            #             logits = outputs
+            #         # TODO: this needs to be fixed and made cleaner later.
+            #         if self.args.past_index >= 0:
+            #             self._past = outputs[self.args.past_index - 1]
+        loss = self.criterion(mean_fused_score, labels)
+
+
+        if prediction_loss_only:
+            return (loss, None, None)
+
+        # logits = nested_detach(logits)
+        # if len(logits) == 1:
+        #     logits = logits[0]
+
+        return (loss, mean_fused_score, labels)
     
     def evaluation_loop(
         self,
@@ -151,8 +243,9 @@ class SGTrainer(Trainer):
                 losses = self.gather_function((loss.repeat(batch_size)))
                 losses_host = losses if losses_host is None else nested_concat(losses_host, losses, padding_index=-100)
             if labels is not None:
-                labels = self.accelerator.pad_across_processes(labels, dim=1, pad_index=-100)
-            if inputs_decode is not None:
+                # labels = self.accelerator.pad_across_processes(labels, dim=1, pad_index=-100)
+                pass
+            if inputs_decode is not None: # NOTE : NOT NEEDED YET
                 inputs_decode = self.accelerator.pad_across_processes(inputs_decode, dim=1, pad_index=-100)
                 inputs_decode = self.gather_function((inputs_decode))
                 inputs_host = (
@@ -165,11 +258,13 @@ class SGTrainer(Trainer):
                 if self.preprocess_logits_for_metrics is not None:
                     logits = self.preprocess_logits_for_metrics(logits, labels)
                 logits = self.gather_function((logits))
-                preds_host = logits if preds_host is None else nested_concat(preds_host, logits, padding_index=-100)
+                # preds_host = logits if preds_host is None else nested_concat(preds_host, logits, padding_index=-100)
+                preds_host = logits if preds_host is None else np.concatenate((preds_host, logits))
 
             if labels is not None:
                 labels = self.gather_function((labels))
-                labels_host = labels if labels_host is None else nested_concat(labels_host, labels, padding_index=-100)
+                # labels_host = labels if labels_host is None else nested_concat(labels_host, labels, padding_index=-100)
+                labels_host = labels if labels_host is None else np.concatenate((labels_host, labels))
 
             self.control = self.callback_handler.on_prediction_step(args, self.state, self.control)
 
@@ -180,7 +275,8 @@ class SGTrainer(Trainer):
                     all_losses = losses if all_losses is None else np.concatenate((all_losses, losses), axis=0)
                 if preds_host is not None:
                     logits = nested_numpify(preds_host)
-                    all_preds = logits if all_preds is None else nested_concat(all_preds, logits, padding_index=-100)
+                    # all_preds = logits if all_preds is None else nested_concat(all_preds, logits, padding_index=-100)
+                    all_preds = logits if all_preds is None else np.concatenate((all_preds, logits))
                 if inputs_host is not None:
                     inputs_decode = nested_numpify(inputs_host)
                     all_inputs = (
@@ -190,8 +286,11 @@ class SGTrainer(Trainer):
                     )
                 if labels_host is not None:
                     labels = nested_numpify(labels_host)
+                    # all_labels = (
+                    #     labels if all_labels is None else nested_concat(all_labels, labels, padding_index=-100)
+                    # )
                     all_labels = (
-                        labels if all_labels is None else nested_concat(all_labels, labels, padding_index=-100)
+                        labels if all_labels is None else np.concatenate((all_labels, labels))
                     )
 
                 # Set back to None to begin a new accumulation
@@ -209,15 +308,17 @@ class SGTrainer(Trainer):
             all_losses = losses if all_losses is None else np.concatenate((all_losses, losses), axis=0)
         if preds_host is not None:
             logits = nested_numpify(preds_host)
-            all_preds = logits if all_preds is None else nested_concat(all_preds, logits, padding_index=-100)
-        if inputs_host is not None:
+            # all_preds = logits if all_preds is None else nested_concat(all_preds, logits, padding_index=-100)
+            all_preds = logits if all_preds is None else np.concatenate((all_preds, logits))
+        if inputs_host is not None: # NOTE : NOT NEEDED YET
             inputs_decode = nested_numpify(inputs_host)
             all_inputs = (
                 inputs_decode if all_inputs is None else nested_concat(all_inputs, inputs_decode, padding_index=-100)
             )
         if labels_host is not None:
             labels = nested_numpify(labels_host)
-            all_labels = labels if all_labels is None else nested_concat(all_labels, labels, padding_index=-100)
+            # all_labels = labels if all_labels is None else nested_concat(all_labels, labels, padding_index=-100)
+            all_labels = labels if all_labels is None else np.concatenate((all_labels, labels))
 
         # Number of samples
         if has_length(eval_dataset):
