d17807eba0f2561240432b5cf95cc97a170be287
Randy <randomzakya@gmail.com>
2024-07-10 20:36:34 +0700

    add hparam tuning analysis

---
+++ paramtuning/sg/to_csv.py
@@ -0,0 +1,38 @@
+import json
+import os
+import pandas as pd
+from tqdm import tqdm
+
+def data_to_row(json_dict):
+    result = {}
+    for k, v in json_dict["hparam"].items():
+        result[k] = v
+    for k, v in json_dict["evaluation_metrics"].items():
+        result[k] = v
+    for i, h in enumerate(json_dict["history"]):
+        for k, v in h.items():
+            if k == "epoch":
+                continue
+            result[f"epoch_{i+1}_{k}"] = v
+    return result
+
+def save_iter(folder):
+    # iter 1
+    df = []
+    for fname in tqdm(os.listdir(folder)):
+        path = os.path.join(folder, fname)
+        json_dict = json.load(open(path))
+        row = data_to_row(json_dict)
+        df.append(row)
+
+    df = pd.DataFrame(df)
+
+    df.to_csv(f"./{folder}.csv", index=False)
+
+    best = df.loc[df["test_sg_f1"].argmax()]
+
+    with open(f"./best_{folder}.json", 'w') as fp:
+        json.dump(best.to_dict(), fp)
+
+save_iter("iter1")
+save_iter("iter2")
